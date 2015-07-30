782a5e4aede6a2922856dcc4b099cf59f6d226e1
chakrsus <sushovan.chakraborty@hp.cm>
2015-07-30 14:28:57 +0530

    added Line 2 on MyMain.java

--- src/MyMain.java
+++ src/MyMain.java
@@ -4,6 +4,7 @@ public class MyMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git test");
+		System.out.println("Git test Line 2");
 
 	}
 
